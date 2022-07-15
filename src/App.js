e1e13cfe04077bf8faf3af1e19adaf1565f396a6
nabilHaouam <workmailnabil@gmail.com>
2022-07-15 09:14:54 +0100

    broken homepage into smaller components and styles them.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import React, { Component } from 'react'
-import {CardList} from './components/cards/card-list.component';
-import { SearchBox } from './components/search-box/search-box.component';
-import './App.css'
+import React from 'react';
+import Homepage from './pages/homepage/homepage.component';
 
-
-export default class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      users:[],
-      searchField:""
-    }
-  }
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({
-      users : users
-    }))
-  }
-  handleChange = e =>(this.setState({searchField: e.target.value}))
-
-  render() {
-    const {searchField , users} = this.state;
-    const filteredUsers = users.filter(user =>
-      user.name.toLowerCase().includes(searchField.toLowerCase()))
-    return (
-      <div className='app'>  
-        <h1 className='app-title'>Aliens Rolodex</h1>     
-        <SearchBox placeholder='search aliens' handleChange={this.handleChange}/>
-        <CardList users={filteredUsers}/>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+        <Homepage/>
+    </div>
+  )
 }
+
+export default App
